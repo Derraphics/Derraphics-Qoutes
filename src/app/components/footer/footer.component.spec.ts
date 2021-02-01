4a3705799f1ea6e4a54262af55553bff47d79989
Derraphics <derrickmuriithi988@gmail.com>
2021-02-01 23:15:16 +0300

    deleted empty lines of codes

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -5,14 +5,12 @@ import { FooterComponent } from './footer.component';
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ FooterComponent ]
     })
     .compileComponents();
   });
-
   beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
