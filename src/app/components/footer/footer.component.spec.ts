97ec01b8b2d289917c0802aa13046ce311713662
Derraphics <derrickmuriithi988@gmail.com>
2021-02-01 23:11:25 +0300

    edited the auther

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -18,7 +18,6 @@ describe('FooterComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
